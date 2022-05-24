6a783981a6dd60f90d3c2517ea990ce4801199ca
Zeynep Yıldırım <zeynepyldrm1516@gmail.com>
2022-05-24 22:20:25 +0300

    initial

---
+++ src/main/java/com/example/vetcase/VetCaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.vetcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetCaseApplication.class, args);
+    }
+
+}
